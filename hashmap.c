ef13c7be3406c1f00ca41f699d398da65193a187
Leiturron <163461718+Leiturron@users.noreply.github.com>
2024-04-25 15:39:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -101,7 +101,7 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) {
   long pos = 0;
-  while(map->buckets[pos]->key == NULL)
+  while(map->buckets[pos] == NULL)
     {
       pos++;
       if(pos == map->capacity) return NULL;
