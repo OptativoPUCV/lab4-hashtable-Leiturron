d3a8606375977e3b29f9e442daa95a99988897eb
Leiturron <163461718+Leiturron@users.noreply.github.com>
2024-04-25 17:51:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,12 +42,22 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) 
 {
   long pos = hash(key, map->capacity);
-  while(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL)
-    {
-      pos++;
-    }
-  map->buckets[pos] = createPair(key, value);
-  map->size++;
+  if(map->buckets[pos] == NULL)
+  {
+    map->buckets[pos] = createPair(key, value);
+    map->size++;
+  }
+  else
+  {
+    while(map->buckets[pos] != NULL)
+      {
+        pos++;
+        if(map->buckets[pos] == NULL)
+          break;
+      }
+    map->buckets[pos] = createPair(key, value);
+    map->size++;
+  }
 }
 
 void enlarge(HashMap * map) {
