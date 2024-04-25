8eef0b733a940ad7a41c0e7a76155abb5a42310f
Leiturron <163461718+Leiturron@users.noreply.github.com>
2024-04-25 16:26:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,9 +52,19 @@ void insertMap(HashMap * map, char * key, void * value)
 }
 
 void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
-
-
+  enlarge_called = 1; //no borrar (testing purposes)
+  Pair** old_buckets = map->buckets;
+  map->capacity *= 2;
+  map->buckets = (Pair**)malloc(map->capacity*sizeof(Pair*));
+  for(int i = 0; i < map->capacity / 2; i++)
+    {
+      if(map->buckets[i] != NULL)
+      {
+        insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
+        free(old_buckets[i]);
+      }
+    }
+  free(old_buckets);
 }
 
 
