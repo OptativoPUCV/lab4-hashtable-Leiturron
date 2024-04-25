311d508ae3b06186aa78012a7a174be8e6f7e4f2
Leiturron <163461718+Leiturron@users.noreply.github.com>
2024-04-25 15:30:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -104,9 +104,9 @@ Pair * firstMap(HashMap * map) {
   while(map->buckets[pos]->key == NULL)
     {
       pos++;
-      if(pos == map->capacity) return NULL;
+      if(map->buckets[pos]->key != NULL) return map->buckets[pos];
     }
-  return map->buckets[pos];
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
